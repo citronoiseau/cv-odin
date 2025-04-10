ae39ab912b3cbadb48fbaff2eafbb46a16f108ed
citronoiseau <avroraka2004@gmail.com>
2025-04-10 18:27:47 +0000

    added photo upload

--- src/components/PersonalForm.jsx
+++ src/components/PersonalForm.jsx
@@ -9,36 +9,65 @@ export default function Form({ formData, setFormData, section }) {
     });
   };
 
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const imageUrl = URL.createObjectURL(file);
+      setFormData((prev) => ({
+        ...prev,
+        personal: {
+          ...prev.personal,
+          photo: imageUrl,
+        },
+      }));
+    }
+  };
+
   return (
-    <form id="personal-form"> 
-      <h2>Personal Info</h2>
+    <form id="personal-form">
+      <div className="inputData">
+        <label htmlFor="name">Full name</label>
+        <input
+          type="text"
+          value={formData.personal.name}
+          onChange={(e) => handleChange("name", e.target.value)}
+          placeholder="Name"
+          id="name"
+        />
+      </div>
 
-      <label htmlFor="email">Full name</label>
-      <input
-        type="text"
-        value={formData.personal.name}
-        onChange={(e) => handleChange("name", e.target.value)}
-        placeholder="Name"
-        id="name"
-      />
+      <div className="inputData">
+        <label htmlFor="email">E-mail</label>
+        <input
+          type="email"
+          value={formData.personal.email}
+          onChange={(e) => handleChange("email", e.target.value)}
+          placeholder="Email"
+          id="email"
+        />
+      </div>
 
-      <label htmlFor="email">E-mail</label>
-      <input
-        type="email"
-        value={formData.personal.email}
-        onChange={(e) => handleChange("email", e.target.value)}
-        placeholder="Email"
-        id="email"
-      />
-      
-      <label htmlFor="phone">Phone</label>
-      <input
-        type="tel"
-        value={formData.personal.phone}
-        onChange={(e) => handleChange("phone", e.target.value)}
-        placeholder="Phone"
-        id="phone"
-      />
+      <div className="inputData">
+        <label htmlFor="phone">Phone</label>
+        <span> optional </span>
+        <input
+          type="tel"
+          value={formData.personal.phone}
+          onChange={(e) => handleChange("phone", e.target.value)}
+          placeholder="Phone"
+          id="phone"
+        />
+      </div>
+
+      <div className="inputData">
+        <label htmlFor="photo">Photo</label>
+        <input
+          type="file"
+          onChange={(e) => handleFileChange(e)}
+          id="photo"
+          accept="image/png, image/jpeg"
+        />
+      </div>
     </form>
   );
 }
--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,50 +1,59 @@
-import '../styles/Preview.css'
+import "../styles/Preview.css";
 
 export default function Preview({ formData }) {
   return (
     <div>
-        <div className="section personal-section">
-            <div>{formData.personal.name}</div> 
-            <div>{formData.personal.email}</div>
-            <div>{formData.personal.phone}</div>
+      <div className="section personal-section">
+        <div>{formData.personal.name}</div>
+        <div>
+          <img
+            src={formData.personal.photo}
+            alt={formData.personal.name}
+            className="personal-photo"
+          />
         </div>
+        <div>{formData.personal.email}</div>
+        <div>{formData.personal.phone}</div>
+      </div>
 
-        <div className="section education-section">
-          {formData.education.map((uni) => (
-            <div className="uni" key={uni.id}>
-              <div>{uni.name}</div>
-              <div>{uni.title}</div>
+      <div className="section education-section">
+        {formData.education.map((uni) => (
+          <div className="uni" key={uni.id}>
+            <div>{uni.name}</div>
+            <div>{uni.title}</div>
+            {uni.startDate !== "" && (
               <div>
                 {new Date(uni.startDate).toLocaleDateString()} –{" "}
-                {uni.endDate
-                  ? new Date(uni.endDate).toLocaleDateString()
-                  : "Present"}
-             </div>
-            </div>
-          ))}
-        </div>
-      
-        <div className="section work-section">
-          {formData.work.map((position) => (
-            <div className="position" key={position.id}>
-              <div>{position.title}</div>
-              <div>{position.place}</div>
-              <div>
-                <ul>
-                  {position.responsibilities.map((item, i) => (
-                    <li key={i}>{item}</li>
-                  ))}
-                </ul>
+                {uni.endDate && new Date(uni.endDate).toLocaleDateString()}
               </div>
-              <div>
-                    {new Date(position.startDate).toLocaleDateString()} –{" "}
-                    {position.endDate
-                      ? new Date(position.endDate).toLocaleDateString()
-                      : "Present"}
-             </div>
+            )}
+          </div>
+        ))}
+      </div>
+
+      <div className="section work-section">
+        {formData.work.map((position) => (
+          <div className="position" key={position.id}>
+            <div>{position.title}</div>
+            <div>{position.place}</div>
+            <div>
+              <ul>
+                {position.responsibilities.map((item, i) => (
+                  <li key={i}>{item}</li>
+                ))}
+              </ul>
             </div>
-          ))}
-        </div>
+            {position.startDate !== "" && (
+              <div>
+                {new Date(position.startDate).toLocaleDateString()} –{" "}
+                {position.endDate
+                  ? new Date(position.endDate).toLocaleDateString()
+                  : "Present"}
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
