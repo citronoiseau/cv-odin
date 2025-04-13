e439620c7401ee68a14a614453ffe8e42f219bd1
citronoiseau <avroraka2004@gmail.com>
2025-04-13 09:54:40 +0000

    added more data

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -3,15 +3,24 @@ export const initialData = {
     name: "Ballerina Capuccina",
     email: "mimimi@mail.com",
     phone: "+39 347 912 3456",
-    photo: "./public/ballerina.png",
+    photo: "/ballerina.png",
+    summary:
+      "Ballerina Capuccina, mi mi mi miiii. È la moglie di Capuccino Assassino. E ama la musica, la la la la! La sua passione e il ballerino lololo.",
   },
   education: [
     {
-      name: "University of Bologna",
-      title: "Theatre Arts",
-      startDate: "2021-01-01",
-      endDate: "2025-06-06",
+      school: "University of Bologna",
+      degree: "Bachelor of Arts in Theatre Arts",
+      startDate: "2015-01-01",
+      endDate: "2020-06-06",
       id: "entry-1",
+    },
+    {
+      school: "University of Milan",
+      degree: "Master of Arts (MA) in Theatre Studies",
+      startDate: "2020-07-02",
+      endDate: "2023-02-03",
+      id: "entry-2",
     },
   ],
   work: [
@@ -19,12 +28,33 @@ export const initialData = {
       title: "Prima Ballerina",
       company: "Italian Opera",
       responsibilities: [
-        { id: "item-1", text: "dancing" },
+        { id: "item-1", text: "dancing with ballerino lololo" },
         { id: "item-2", text: "drinking cappuccino" },
+        {
+          id: "item-3",
+          text: "convincing the audience I'm actually floating",
+        },
       ],
-      startDate: "2025-10-01",
+      startDate: "2023-01-01",
       endDate: null,
       id: "entry-1",
+    },
+    {
+      title: "Dance Teacher",
+      company: "Italian Opera",
+      responsibilities: [
+        {
+          id: "item-1",
+          text: "teaching Espressona Seniora to dance with passion",
+        },
+        {
+          id: "item-2",
+          text: "encouraging students to embrace their inner pasta twirls",
+        },
+      ],
+      startDate: "2017-07-08",
+      endDate: "2022-05-10",
+      id: "entry-2",
     },
   ],
 };
